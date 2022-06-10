5a8834381293513361bb147b4b01148fd7fd78cd
M-mawali <muhmadmawali@gmail.com>
2022-06-10 18:27:07 +0200

    added the search functionality

--- my-finder/src/componets/RestaurantsList.js
+++ my-finder/src/componets/RestaurantsList.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 
 function RestaurantsList (){//api fatching
     const [restaurants,setRestaurants] =useState([]);
+    const [filteredRstaurants,setFilteredRestaurants] =useState([]);
     
     useEffect(()=>{
         async function dataFetch () {
@@ -13,6 +14,17 @@ function RestaurantsList (){//api fatching
         dataFetch()
     },[]);
     console.log(restaurants);
+
+    const onSearchedRestaurants = (e) => {
+        const search = e.target.value;
+
+        const searchedRestaurants = restaurants.filter((restaurant) =>
+        restaurant.name.toLowerCase().includes(search.toLowerCase()) ||  restaurant.cuisine.includes(search.toLowerCase())
+        );
+        setFilteredRestaurants(searchedRestaurants);
+        console.log(filteredRstaurants)
+        };
+    
     return(
         <>
             <div className='search-div'>
@@ -22,15 +34,16 @@ function RestaurantsList (){//api fatching
                     autoComplete='off'
                     type='text'
                     placeholder='type restaurant name or cuisine '
+                    onChange={onSearchedRestaurants}
                 />
             </div>
 
         <div className='restaurants'>
-        {restaurants.map((restaurant) => (//restaurants list creating
+        {filteredRstaurants && filteredRstaurants.map((restaurant) => (//restaurants list creating
             <div className='restaurant' key={restaurant.id}>
                     <br/>    
                     <div style={{fontSize:'15px'}}><h3>{restaurant.name}</h3></div>
-                    <div> Rate: {restaurant.rating} ({restaurant.user_ratings_total}) <h3>{restaurant.cuisine}</h3></div>
+                    <div> Rate: <span style={{color:'gold'}}> {restaurant.rating} </span> ({restaurant.user_ratings_total}) <h3>{restaurant.cuisine}</h3></div>
                     <div className='res-img'><img src={restaurant.photos[0].links[1]} alt='' /></div>
                     <div><h3>Address:</h3>{restaurant.formatted_address}</div>
             </div>  )
