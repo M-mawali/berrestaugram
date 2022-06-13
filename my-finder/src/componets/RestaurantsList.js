be5ec612994d1f204d670b19171a3667b8bb9236
M-mawali <muhmadmawali@gmail.com>
2022-06-13 16:18:25 +0200

    added filters (fuctionality , a bit of styling)

--- my-finder/src/componets/RestaurantsList.js
+++ my-finder/src/componets/RestaurantsList.js
@@ -6,6 +6,9 @@ import { Link } from 'react-router-dom';
 function RestaurantsList (){//api fatching
     const [restaurants,setRestaurants] =useState([]);
     const [filteredRstaurants,setFilteredRestaurants] =useState([]);
+    const [filteredRstaurants2,setFilteredRestaurants2] =useState([]);
+    const [filteredRstaurants3,setFilteredRestaurants3] =useState([]);
+    
     
     useEffect(()=>{
         async function dataFetch () {
@@ -17,15 +20,31 @@ function RestaurantsList (){//api fatching
     },[]);
     console.log(restaurants);
 
-    const onChkClick = (e) => {//checkbox results handler
-        const name   =e.target
 
-        const chkcuisine = restaurants.filter((restaurant)=>
-            restaurant.cuisine.toLowerCase().includes(name.toLowerCase())) ;
-        setFilteredRestaurants(chkcuisine)
+    const onSelect = (e ) => {//checkbox results handler
+        const name   =e.target.value
+        
+        const selector = restaurants.filter((restaurant)=>
+            restaurant.cuisine.toLowerCase().includes(name.toLowerCase()) ) 
+            setFilteredRestaurants2(selector)
+            setFilteredRestaurants(selector)
         console.log(filteredRstaurants)
-    }
+        console.log(name)
+        
+    } 
 
+    const onSelect2 = (e ) => {//checkbox results handler
+        const name   =e.target.value
+    const  selector2 =  filteredRstaurants2.filter((restaurant ) =>
+     restaurant.dietaryRestrictions.toLowerCase().includes(name.toLowerCase())) 
+       setFilteredRestaurants(selector2)
+
+    const selector3 =restaurants.filter((restaurant ) =>
+     restaurant.dietaryRestrictions.toLowerCase().includes(name.toLowerCase()))    
+        setFilteredRestaurants3(selector3)
+        console.log(name)
+
+    }
     const onSearchedRestaurants = (e) => {// search results handler
         const search = e.target.value;
 
@@ -48,16 +67,34 @@ function RestaurantsList (){//api fatching
                     placeholder='type restaurant name or cuisine '
                     onChange={onSearchedRestaurants}
                 />
-                <div> 
-                <input type='checkbox' name='italian' onChange={onChkClick}/>italian&nbsp;&nbsp;
-                <input type='checkbox' name='thai' onChange={onChkClick} />thai&nbsp;&nbsp;
-                <input type='checkbox' name='american' onChange={onChkClick} />american&nbsp;&nbsp;
-                <input type='checkbox' name='vietnameese' onChange={onChkClick} />vietnameese&nbsp;&nbsp;
-                <input type='checkbox' name='indian' onChange={onChkClick} />indian&nbsp;&nbsp;
-                <input type='checkbox' name='chinese' onChange={onChkClick} />chinese&nbsp;&nbsp;
-                <input type='checkbox' name='German' onChange={onChkClick} />german&nbsp;&nbsp;
-                <input type='checkbox' name='mexican' onChange={onChkClick} />mexican&nbsp;&nbsp;
-                </div>
+                <div className='selectors'> 
+                        <div>
+                  <label for='cuisine-select'>Filter 1 </label>
+                  <select onChange={onSelect} name="cuisine"  id='cuisine-select' className='selector'>
+                  <option name='' >no select</option>
+                      <option name='italian' >italian</option>
+                      <option name='thai'>thai</option>
+                      <option name='american'>american</option>
+                      <option name='vietnameese'>vietnameese</option>
+                      <option name='indian'>indian</option>
+                      <option name='chinese'>chinese</option>
+                      <option name='German'>German</option>
+                      <option name='mexican'>mexican</option>
+                      </select>
+                      </div>
+                      <div>
+                      <label for='dietaryRestrictions-select'>Filter 2 </label>
+                      <select onChange={ onSelect2} name="dietaryRestrictions"  id='dietaryRestrictions-select' className='selector' >
+                      <option name='no select' id='0'>no select</option>    
+                      <option name='halal' >halal</option>
+                      <option name='kosher' >kosher</option>
+                      <option name='gluten free' >gluten free</option>
+                      <option name='lactose free' >lactose free</option>
+                      <option name='vegan' >vegan</option>
+                      <option name='vegetarian' >vegetarian</option>
+                      </select>
+                      </div>
+                      </div>
             </div>
 
             <div>
@@ -82,6 +119,22 @@ function RestaurantsList (){//api fatching
                     </div>
                     </Link>
             </div>  ) 
+        )}  
+        </div>
+        <div className='restaurants'>
+        {filteredRstaurants3.map((restaurant) => (//restaurants list creating
+            <div className='restaurant' key={restaurant.id}>
+                <Link to={`/RestaurantsList/${restaurant.id}`}>
+                    <br/>    
+                    <div style={{fontSize:'15px'}}><h3>{restaurant.name}</h3></div>
+                    <div> Rate: <span style={{color:'gold'}}> {restaurant.rating} </span> ({restaurant.user_ratings_total}) <h3>{restaurant.cuisine} <span style={{color:'blue'}}>({restaurant.dietaryRestrictions})</span></h3></div>
+                    <div className='res-img'><img src={restaurant.photos[0].links[1]} alt='' /></div>
+                    <div className='open-now'>
+                        {filteredRstaurants3 && restaurant && restaurant.opening_hours &&
+                            restaurant.opening_hours.open_now && <h3> Open Now</h3>}
+                    </div>
+                    </Link>
+            </div>)
         )}
         </div>
         </>)}
